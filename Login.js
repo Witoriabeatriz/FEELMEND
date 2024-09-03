63bf0cf0257d28a8785317f712692de2c8e09fa7
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-09-03 07:21:28 -0300

    correção

---
+++ Login.js
@@ -0,0 +1,15 @@
+
+// BLOCO DE VERIFICAÇÃO DE PREENCHIMENTO DE CAMPOS
+function Verifica() {
+    let user = document.getElementById('loginEmail').value;
+    let senha = document.getElementById('loginSenha').value;
+    if (!user || !senha) {
+    alert("Campos de preenchimento obrigatorio. Favor preencher!");
+    }else {
+        window.location = "./Campeonato.html";}}
+
+
+// REDIRECIONAR PARA CADASTRO
+function Redirecionar() {
+window.location.href = "Cadastro.html";
+}
